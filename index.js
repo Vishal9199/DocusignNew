6e529a5ea2870bf9fd434e025b4cc70c62b16858
Vishal Kumar Mahatha <76895316+Vishal9199@users.noreply.github.com>
2025-02-08 15:34:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -107,8 +107,7 @@ app.get("/", async (request, response) => {
 
 
 app.get("/success", (request, response) => {
-   response.send(`<h2>Signature Completed!</h2>
-        <p>Your document has been signed successfully.</p>`);
+   response.send("Successfully Signed");
 });
 
 // https://account-d.docusign.com/oauth/auth?response_type=code&scope=signature%20impersonation&client_id=(YOUR CLIENT ID)&redirect_uri=http://localhost:8000/
